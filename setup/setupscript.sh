0688aeeb0a26dfc1144128edabc8aac0ead70ec7
sf <simon.farre.cx@gmail.com>
2020-04-07 18:19:40 +0200

    Updated the setupscript.sh

--- setup/setupscript.sh
+++ setup/setupscript.sh
@@ -3,19 +3,17 @@
 cd /home/cx/dev/packages
 # /TODO
 
-wget https://github.com/mongodb/mongo-c-driver/releases/download/1.15.3/mongo-c-driver-1.15.3.tar.gz
-tar xzf mongo-c-driver-1.15.3.tar.gz
-mv mongo-c-driver-1.15.3.tar.gz ./archives
-cd mongo-c-driver-1.15.3
-mkdir cmake-build
-cd cmake-build
-cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
-make
-sudo make install
-# TODO 1:  change this, so we can take a parameter for a dir to cd in to...
-cd /home/cx/dev/packages/
-# /TODO
-git clone https://github.com/mongodb/mongo-cxx-driver.git \
-    --branch releases/stable --depth 1
-cd mongo-cxx-driver/build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_COMPILER=gcc-8 -DBUILD_SHARED_AND_STATIC_LIBS=ON
+check_for_cargo() {
+    if hash cargo 2>/dev/null; then
+        echo "cargo is installed. nothing needs to be done."
+    else
+        echo "Cargo and (possibly) the rust toolchain not installed... Downloading..."
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    fi
+}
+
+check_for_cargo
+
+cd hockeyscraper
+cargo build
+cargo run -- .
