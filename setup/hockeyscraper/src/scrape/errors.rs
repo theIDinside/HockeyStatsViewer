1422a945e46495f168d63e6e2642991678e20e9c
Simon Farre <simon.farre.cx@gmail.com>
2023-02-10 16:15:11 +0100

    Removed old formatter

--- setup/hockeyscraper/src/scrape/errors.rs
+++ setup/hockeyscraper/src/scrape/errors.rs
@@ -1,5 +1,4 @@
 use reqwest::Error;
-use serde::export::Formatter;
 
 /// These are type aliases meant for documentation of use for BuilderError
 pub type Message = &'static str;
@@ -17,7 +16,7 @@ pub enum BuilderError {
 }
 
 impl std::fmt::Display for BuilderError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             BuilderError::REQWEST(req_err) => {
                 write!(f, "{}", req_err)
